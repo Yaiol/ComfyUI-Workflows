174a7650e4d2579cc5f0a2a39d5e6e6e9a58b735
Yaiol <38218161+Yaiol@users.noreply.github.com>
2024-11-25 12:41:55 +0100

    rename

--- workflows/sort.cmd
+++ workflows/sort.cmd
@@ -5,7 +5,7 @@ pushd "%d1%"
 pushd "%d2%"
 for %%d in (sdxl,pyxl,sdlt,sdtr,sdca,sd30,sd35,fx10,kl10) do ( 
   for /F %%f in ('dir /b *-%%d*.*') do (
-    move "%%f" "..\..\%%d\%d1%\%d2%"
+    git mv "%%f" "..\..\%%d\%d1%\%d2%"
   )
 )
 popd
