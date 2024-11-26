640c91f045049aebd2ffdd465dd25d53a25c42b2
Yaiol <38218161+Yaiol@users.noreply.github.com>
2024-11-27 00:11:14 +0100

    rename

--- workflows/create.cmd
+++ workflows/create.cmd
@@ -1,16 +1,22 @@
-for %%d in (sdxl,pyxl,sdlt,sdtr,sdca,sd30,sd35,fx10,kl10) do ( 
+for %%d in (fx10,kl10,sdxl,pyxl,sdlt,sdtr,sdca,sd30,sd35) do ( 
   mkdir %%d
   pushd %%d
+  
   mkdir "01 txt2img"
   pushd "01 txt2img"
   mkdir "00 txt2img-basic"
+  mkdir "01 txt2img-hires"
+  mkdir "02 txt2img-cond"
+  mkdir "20 txt2img-ipa"
   popd
+
   mkdir "02 img2img"
   pushd "02 img2img"
-  mkdir "00 txt2img-basic"
   popd
+
   mkdir "03 inpaint"
-  mkdir "04 outpaint"
-  mkdir "30 face2img"
+  pushd "03 inpaint"
   popd
+  
+  popd  
 )
